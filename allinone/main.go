5a984f2749c994221f64a810300cfc5e266e8b81
Dov Murik <dov.murik1@il.ibm.com>
2019-12-05 13:03:36 +0000

    allinone: Register required handlers
    The logstatus ManglerHandler and the secadvisor TransformerHandler were
    not registered in allinone, and therefore the following errors were
    given when starting with ./allinone.yml.default:
    
        ERROR   core/main.go:45 Main    : Failed to initialize pipeline: mangle type logstatus not supported
    
        ERROR   core/main.go:45 Main    : Failed to initialize pipeline: transform type secadvisor not supported

--- allinone/main.go
+++ allinone/main.go
@@ -28,7 +28,8 @@ func main() {
 }
 
 func init() {
+	core.ManglerHandlers.Register("logstatus", secadvisor.NewMangleLogStatus, false)
 	core.EncoderHandlers.Register("secadvisor", secadvisor.NewEncode, false)
 	core.TransformerHandlers.Register("awsflowlogs", awsflowlogs.NewTransform, false)
-	core.TransformerHandlers.Register("vpclogs", secadvisor.NewTransform, false)
+	core.TransformerHandlers.Register("secadvisor", secadvisor.NewTransform, false)
 }
